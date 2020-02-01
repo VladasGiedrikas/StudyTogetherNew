a7e39946f2e49d06281e22a53e2097e06715abb9
VladasGiedrikas <31672841+VladasGiedrikas@users.noreply.github.com>
2020-02-01 20:49:59 +0200

    Add project files.

---
+++ Models/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyTogetherNew.Models
+{
+    public class Quiz
+    {
+        List<Quiz> quizes;
+
+        public string Question { get; set; }
+        public List<string> Answers { get; set; }
+        public int IndexCorrectAnswer { get; set; }
+    }
+}
